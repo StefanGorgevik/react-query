2d717fbcd7c77f520b61b01335133e9059a7352c
Stefan Gjorgjevikj <stefan.gjorgjevikj@ibm.com>
2021-10-03 11:36:37 +0200

    fixes

--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -12,19 +12,15 @@ const fetchPlanets = async (props) => { //key --first item in array; someVar is 
 const Planets = () => {
   const [page, setPage] = useState(1);
 
-  // const {data, status} = useQuery(['planets', page], fetchPlanets, { //use array for first arg if you want to send query parameters to the function, the first item in the array is always the key
-  //   staleTime: 2000, //from fresh to stale in 2 sec
-  //   retry: 3, //how many times to retry after error, 3 default
-  //   cacheTime: 10, //how long stale queries are cached for before they are disposed, 300 000 default(5 min)
-  //   onSuccess: () => console.log('Will fire on success'), // run when success,
-  //   onError: () => console.log("When error")
-  // }); // 1 key for the name of the data, 2 function(async) to get data; 3. config object
+  const {data, status, isPreviousData} = useQuery(['planets', page], fetchPlanets, { //use array for first arg if you want to send query parameters to the function, the first item in the array is always the key
+    staleTime: 2000, //from fresh to stale in 2 sec
+    retry: 3, //how many times to retry after error, 3 default
+    cacheTime: 10, //how long stale queries are cached for before they are disposed, 300 000 default(5 min)
+    onSuccess: () => console.log('Will fire on success'), // run when success,
+    onError: () => console.log("When error")
+  }); // 1 key for the name of the data, 2 function(async) to get data; 3. config object
+  console.log(data)
 
-  const { 
-    resolvedData, 
-    latestData, 
-    status 
-  } = usePaginatedQuery(['planets', page], fetchPlanets);
   return (
     <div>
       <h2>Planets</h2>
@@ -44,13 +40,18 @@ const Planets = () => {
             Previous Page
           </button>
           <span>{ page }</span>
-          <button 
-            onClick={() => setPage(old => (!latestData || !latestData.next ? old : old + 1))} 
-            disabled={!latestData || !latestData.next}>
-            Next page
+          <button
+         onClick={() => {
+           if (!isPreviousData && data.hasMore) {
+             setPage(old => old + 1)
+           }
+         }}
+         // Disable the Next Page button until we know a next page is available
+         disabled={isPreviousData || !data?.hasMore}
+       > Next page
           </button>
           <div>
-            { resolvedData.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
+            { data.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
           </div>
         </>
       )} 
