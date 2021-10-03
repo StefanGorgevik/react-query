ff7a5b1c8b6a2be1271b8e28bbd4ecb5ab410a9c
Stefan Gjorgjevikj <stefan.gjorgjevikj@ibm.com>
2021-10-03 09:53:38 +0200

    finished lesson 1 and 2

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {useQuery} from 'react-query';
+import Person from './Person';
+
+const fetchPeople = async () => {
+  const res = await fetch('https://swapi.dev/api/people/'); //if you have an error, it will try to fetch three times and then return status error
+  return res.json();
+}
 
 const People = () => {
+  const {data, status} = useQuery('people', fetchPeople); //key for the name of the data, function(async) to get data
+  console.log(data, status)
   return (
     <div>
       <h2>People</h2>
+
+      {status === 'loading' && (
+        <div>Loading data...</div>
+      )}
+      {status === 'error' && (
+        <div>Error fetching data </div>
+      )}
+       {status === 'success' && (
+        <div>
+          {data.results.map(person => <Person person={person}/> )}
+        </div>
+      )}
     </div>
   );
 }
--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {useQuery} from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets = async () => {
+  const res = await fetch('https://swapi.dev/api/planets'); //if you have an error, it will try to fetch three times and then return status error
+  return res.json();
+}
 
 const Planets = () => {
+  const {data, status} = useQuery('planets', fetchPlanets); //key for the name of the data, function(async) to get data
+  console.log(data, status)
   return (
     <div>
       <h2>Planets</h2>
+
+      {status === 'loading' && (
+        <div>Loading data...</div>
+      )}
+      {status === 'error' && (
+        <div>Error fetching data </div>
+      )}
+       {status === 'success' && (
+        <div>
+          {data.results.map(planet => <Planet planet={planet}/> )}
+        </div>
+      )}
     </div>
   );
 }
